bd14f19e1ba654de43ec13013f44e612c185e9e5
Alex Marquez <marquez798@gmail.com>
2023-05-23 16:03:06 -0500

    added list of questions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const shapes = require('./lib/shapes')
+const inquirer = require('inquirer');
+
+
+
+//QUESTIONS TO ASK USER
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Enter (3) characters please"
+    },
+    {
+        type: "input",
+        name: "text-color",
+        message: "text-color: please input color keyword"
+    },
+    {
+        type:"input",
+        name: "shape",
+        message: "Shape-color: please input shape color keyword"
+    },
+    {
+        type:"list",
+        name: "pixel-image",
+        message: "Chose your shape",
+        choices: ("Circle","Square","Triangle")
+    },
+
+]
