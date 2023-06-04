4f0a9bbc36bcaaaae069140e78e08c0dd459c852
Alex Marquez <marquez798@gmail.com>
2023-06-04 18:07:07 -0500

    added a switch statement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var inquirer = require('inquirer');
 const fs = require('fs');
-const generateLogo = require('./lib/generateLogo')
+// const generateLogo = require('./lib/generateLogo')
+const {Shape, Triangle, Circle, Square} =require('./lib/shapes')
 
 
 
@@ -10,45 +11,77 @@ const questions = [
     {
         type: "input",
         name: "text",
-        message: "Enter (3) characters please"
+        message: "Enter (3) characters please",
+        validate: (input) => input.length < 3,
     },
     {
         type: "input",
-        name: "text-color",
+        name: "textColor",
         message: "text_color: please input color keyword"
     },
     {
         type:"input",
         name: "shape",
-        message: "Shape_color: please input shape color keyword"
+        message: "ShapeColor: please input shape color keyword"
     },
     {
         type:"list",
-        name: "pixel-image",
+        name: "shape",
         message: "Chose your shape",
         choices: ["Circle","Square","Triangle"]
     },
 ];
 
-//TAKES IN CONTENTS OF FILENAME, DATA TO WRITE TO FILE
-function writeToFile(fileName, data){
-    var content = generateLogo(data);
-    fs.writeFile(fileName, content, function(error){
-        if (error){
-            return console.log(error);
-        }
-        console.log('GENERATED logo.svg');
-    });
+inquirer.Prompt(questions).then((answers)) => {
+    const {text, textColor, shape, shapeColor} = answers;
+    const shapeObj = new shape()
+    let svgElement = '';
 
-};
-
-//FUNCTION TO PROMPT THE USER
-    function init(){
-        inquirer.prompt(questions).then(function (data){
-            var fileName = 'logo.svg';
-            writeToFile(fileName, data);
-        })
+    shapeObj.setColor(shapeColor);
+    
+    switch(shape){
+        case 'Triangle':
+            const Triangle = new Triangle();
+            Triangle.setColor(shapeColor);
     }
+}
 
-    //CALL TO INIT THE APP
-    init();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //TAKES IN CONTENTS OF FILENAME, DATA TO WRITE TO FILE
+// function writeToFile(fileName, data){
+//     var content = generateLogo(data);
+//     fs.writeFile(fileName, content, function(error){
+//         if (error){
+//             return console.log(error);
+//         }
+//         console.log('GENERATED logo.svg');
+//     });
+
+// };
+
+// //FUNCTION TO PROMPT THE USER
+//     function init(){
+//         inquirer.prompt(questions).then(function (data){
+//             var fileName = 'logo.svg';
+//             writeToFile(fileName, data);
+//         })
+//     }
+
+//     //CALL TO INIT THE APP
+//     init();
